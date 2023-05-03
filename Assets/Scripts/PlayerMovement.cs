05a4afc6cf3b4a55b131b19aae3156e8cdc81bab
Juan Daniel Muñoz Dueñas <88680309+DanielMunoz4190@users.noreply.github.com>
2023-05-02 19:56:44 -0600

    text

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -36,12 +36,17 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private AudioSource deathSound;
 
+
+
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         playerHealth = GetComponent<PlayerHealth>();
         Invoke("perder", 5.0f);
+
+
     }
 
     void perder()
@@ -60,6 +65,8 @@ public class PlayerMovement : MonoBehaviour
         {
             Jump();
         }
+
+
     }
 
     void OnCollisionEnter(Collision collision)
