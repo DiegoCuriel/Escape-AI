48a2deb5379f073b50102b844555f1d35846804a
DiegoCuriel <a01640372@itesm.mx>
2023-05-02 21:38:57 -0600

    Juego Final - enemigos agregados

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -6,13 +6,18 @@ using UnityEngine.AI;
 public class EnemyFollow : MonoBehaviour
 {
     public NavMeshAgent enemy;
-    
     public Transform player;
+    public float maxDistance = 20f; // distancia máxima permitida para seguir al jugador
 
     void Start() {}
 
     void Update()
     {
-        enemy.SetDestination(player.position);
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        // Verificar si el enemigo está dentro del radio de distancia permitido
+        if (distanceToPlayer <= maxDistance) {
+            enemy.SetDestination(player.position);
+        }
     }
 }
